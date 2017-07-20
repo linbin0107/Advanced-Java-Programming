ac12541435e637b8abf226ebfd223101f97038e3
BIN LIN <linbinmr@gmail.com>
2017-07-20 00:01:22 -0700

    Implement numbers to English words

--- NumbersToWordsKata/number2words/src/main/java/edu/pdx/cs410J/numbers2words/NumbersToWords.java
+++ NumbersToWordsKata/number2words/src/main/java/edu/pdx/cs410J/numbers2words/NumbersToWords.java
@@ -1,15 +1,94 @@
 package edu.pdx.cs410J.numbers2words;
 
+import java.util.HashMap;
+
 public class NumbersToWords {
+    private HashMap<Integer, String> map = new HashMap<Integer, String>();
 
-    public String parse(String input) throws IllegalArgumentException {
+    public String numberToWords(int number) {
+        buildMap();
 
-        if(input.equals("1")) {
-            return "One Dollar";
+        String res = "";
+
+        if (number >= 1000000000) {
+            int quotient = number / 1000000000;
+            res += converter(quotient) + " Billion";
+            number %= 1000000000;
         }
-        else if(input.equals("2")){
-            return "Two";
+
+        if (number >= 1000000) {
+            int quotient = number / 1000000;
+            res += converter(quotient) + " Million";
+            number %= 1000000;
         }
-        throw new IllegalArgumentException("Bad argument: " + input);
+
+        if (number >= 1000) {
+            int quotient = number / 1000;
+            res += converter(quotient) + " Thousand";
+            number %= 1000;
+        }
+
+        if (number > 0) {
+            res += converter(number);
+        }
+
+        return null;
+    }
+
+    private String converter(int quotient) {
+        String str = "";
+
+        if (quotient >= 100) {
+            int hundred = quotient / 100;
+            str += map.get(hundred) + " Hundred";
+            quotient %= 100;
+        }
+
+        if (quotient > 0) {
+            if (quotient > 0 && quotient <= 20) {
+                str += " " + map.get(quotient);
+            } else {
+                int tens = quotient / 10;
+                str += " " + map.get(tens * 10);
+
+                int digit = quotient % 10;
+                if(digit>0){
+                    str += " " + map.get(digit);
+                }
+            }
+        }
+
+        return str;
+    }
+
+    private void buildMap() {
+        map.put(0, "Zero");
+        map.put(1, "One");
+        map.put(2, "Two");
+        map.put(3, "Three");
+        map.put(4, "Four");
+        map.put(5, "Five");
+        map.put(6, "Six");
+        map.put(7, "Seven");
+        map.put(8, "Eight");
+        map.put(9, "Night");
+        map.put(10, "Ten");
+        map.put(11, "Eleven");
+        map.put(12, "Twelve");
+        map.put(13, "Thirteen");
+        map.put(14, "Fourteen");
+        map.put(15, "Fifteen");
+        map.put(16, "Sixteen");
+        map.put(17, "Seventeen");
+        map.put(18, "Eighteen");
+        map.put(19,"Nineteen");
+        map.put(20, "Twenty");
+        map.put(30, "Thirty");
+        map.put(40, "Forty");
+        map.put(50, "Fifty");
+        map.put(60, "Sixty");
+        map.put(70, "Seventy");
+        map.put(80, "Eighty");
+        map.put(90, "Nighty");
     }
 }
--- NumbersToWordsKata/number2words/src/test/java/edu/pdx/cs410J/numbers2words/NumbersToWordsTest.java
+++ NumbersToWordsKata/number2words/src/test/java/edu/pdx/cs410J/numbers2words/NumbersToWordsTest.java
@@ -1,36 +1,16 @@
 package edu.pdx.cs410J.numbers2words;
 
-import org.junit.Before;
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Unit tests for the {@link NumbersToWords} class.
  */
 public class NumbersToWordsTest {
-
-    private NumbersToWords ntw;
-
-    @Before
-    public void setUp() throws Exception {
-        ntw = new NumbersToWords();
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void doesNotSupportNonNumbers() {
-        ntw.parse("chicken");
-    }
-
     @Test
-    public void testSingleStringToNumber(){
-        assertThat(ntw.parse("1"), equalTo("One Dollar"));
+    public void numberZeroReturnZero() {
+        NumbersToWords ntw = new NumbersToWords();
+        assertEquals(ntw.converter(0), "Zero");
     }
-
-    @Test
-    public void number2ReturnsTwo(){
-        assertThat(ntw.parse("2"), equalTo("Two"));
-    }
-  
 }
