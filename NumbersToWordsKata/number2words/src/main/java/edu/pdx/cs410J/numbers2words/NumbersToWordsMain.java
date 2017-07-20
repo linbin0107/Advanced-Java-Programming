e0fc7149fe6c9efbddcbc5bdf44c4a4b1f695a5a
Bin Lin <linbinmr@gmail.com>
2017-07-20 11:18:15 -0700

    Update NumbersToWordsMain.java

--- NumbersToWordsKata/number2words/src/main/java/edu/pdx/cs410J/numbers2words/NumbersToWordsMain.java
+++ NumbersToWordsKata/number2words/src/main/java/edu/pdx/cs410J/numbers2words/NumbersToWordsMain.java
@@ -8,7 +8,8 @@ import edu.pdx.cs410J.AbstractAirline;
 public class NumbersToWordsMain {
 
   public static void main(String[] args) {
-    Class c = AbstractAirline.class;  // Refer to one of Dave's classes so that we can be sure it is on the classpath
+    // Refer to one of Dave's classes so that we can be sure it is on the classpath
+    Class c = AbstractAirline.class;  
     System.err.println("Missing command line arguments");
     for (String arg : args) {
       System.out.println(arg);
